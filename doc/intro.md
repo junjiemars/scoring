7ef1a0fbafb4b44d741a29a606c4ce11726c5ab4
南山 <junjiemars@gmail.com>
2015-07-21 10:52:55 +0800

    init repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to scoring
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/scoring/core_test.clj
@@ -0,0 +1,7 @@
+(ns scoring.core-test
+  (:require [clojure.test :refer :all]
+            [scoring.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
